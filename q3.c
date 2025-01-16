70041cf861c48a00517cea348453e54bb24ea379
dolev goaz <www.dolev@gmail.com>
2025-01-16 22:11:52 +0200

    q3 file

---
+++ q3.c
@@ -0,0 +1,107 @@
+typedef enum {
+    ID = 1,
+    FOR_RANGE,
+    TO,
+    STEP,
+    NUM,
+
+    OPEN_BRACKET,
+    CLOSE_BRACKET,
+    EQ,
+    SEMICOL,
+
+    // extra redundant tokens
+    IF,
+    WHILE
+} Token;
+
+typedef int LABEL;
+typedef char* VARIABLE;
+
+typedef union {
+    VARIABLE var;
+    int ivalue;
+} LexicalValue;
+
+Token lookahead;
+LexicalValue lexicalValue;
+
+VARIABLE newtemp();
+LABEL newlabel();
+void label(LABEL);
+void match(Token);
+void error();
+
+void gen(const char *format, ...);
+
+typedef struct {
+    LABEL cond_label;
+    LABEL loop_label;
+    LABEL finish_label;
+    VARIABLE cond_var;
+} Meta;
+
+VARIABLE expression();
+
+Meta meta() {
+    return (Meta){
+        .cond_label=newlabel(),
+        .loop_label=newlabel(),
+        .finish_label=newlabel(),
+        .cond_var=newtemp(),
+    };
+}
+
+void for_range() {
+    match(FOR_RANGE);
+    match(OPEN_BRACKET);
+    if (lookahead != ID) error();
+    
+    VARIABLE counter = lexicalValue.var;
+    match(ID);
+    match(EQ);
+
+    VARIABLE initial_value = expression();
+    match(TO);
+    VARIABLE max_value = expression();
+
+    Meta for_meta = meta();
+
+    gen("IASN %s %s\n", counter, initial_value);                    // counter = initial_value
+    label(for_meta.cond_label);
+    gen("IGRT %s %s %s\n", for_meta.cond_var, counter, max_value);  // cond_var = (counter > max_value)
+    gen("JUMPZ L%d %s\n", for_meta.loop_label, for_meta.cond_var);  // if (!cond_var): loop
+    gen("JUMP L%d\n", for_meta.finish_label);                       // else: end loop
+    label(for_meta.loop_label);
+
+    match(SEMICOL);
+    match(STEP);
+    if (lookahead != NUM) error();
+    int step = lexicalValue.ivalue;
+    match(NUM);
+    match(CLOSE_BRACKET);
+
+    stmt();
+    gen("IADD %s %s %d\n", counter, counter, step); // counter += step
+    gen("JUMP L%d\n", for_meta.cond_label);         // back to loop condition
+    label(for_meta.finish_label);
+
+    /* free VARIABLEs/LABELs if needed */
+}
+void stmt() {
+    switch (lookahead) {
+        case WHILE: /*...*/ break;
+        case ID: /*...*/ break;
+        case IF: /*...*/ break;
+        case FOR_RANGE:
+            for_range();
+            return;
+        
+        /*...*/
+    }
+}
+
+int main() {
+    stmt();
+    return 0;
+}
